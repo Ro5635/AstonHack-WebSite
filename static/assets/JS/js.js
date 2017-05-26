0d9e4c07d1558773a67e6444bb613592573d9713
Robert Curran <ro5635@gmail.com>
2017-05-26 01:05:00 +0100

    First Commit With AstonHack Site Converted from the ACSS Site Repo
    Added a Footer
    Added a Header ('Hero' Style)
    Rewrote JS scripts for Parralx Effect on header and footer
    Added new Media
    Several Versions of the main building exist, latest iteration has highest number
    SASS file re-written from ACSS base file
    
     Changes to be committed:
    	modified:   assets/JS/js.js
    	new file:   assets/media/AstonHack2016Image1.jpg
    	new file:   assets/media/AstonHack2016Image10-web.jpg
    	new file:   assets/media/AstonHack2016Image10.jpg
    	new file:   assets/media/AstonHack2016Image11-web.jpg
    	new file:   assets/media/AstonHack2016Image11.jpg
    	new file:   assets/media/AstonHack2016Image12.jpg
    	new file:   assets/media/AstonHack2016Image13.jpg
    	new file:   assets/media/AstonHack2016Image14.jpg
    	new file:   assets/media/AstonHack2016Image15.jpg
    	new file:   assets/media/AstonHack2016Image3.jpg
    	new file:   assets/media/AstonHack2016Image4.jpg
    	new file:   assets/media/AstonHack2016Image5-lg.jpg
    	new file:   assets/media/AstonHack2016Image5-md.jpg
    	new file:   assets/media/AstonHack2016Image5-sm.jpg
    	new file:   assets/media/AstonHack2016Image5-web.jpg
    	new file:   assets/media/AstonHack2016Image5.jpg
    	new file:   assets/media/AstonHack2016Image6.jpg
    	new file:   assets/media/AstonHack2016Image7.jpg
    	new file:   assets/media/AstonHack2016Image8.jpg
    	new file:   assets/media/AstonHack2016Image9-web.jpg
    	new file:   assets/media/AstonHack2016Image9.jpg
    	new file:   assets/media/AstonHack2016OR.jpg
    	new file:   assets/media/AstonStudentsUnionLogo.png
    	new file:   assets/media/BGVid.mp4
    	new file:   assets/media/BGVid.ogg
    	new file:   assets/media/BGVid.webm
    	new file:   assets/media/StaticBGVidImage.JPG
    	new file:   assets/media/StaticBGVidImageLowerSegment.JPG
    	new file:   assets/media/StaticBGVidImageUpperSegment.JPG
    	new file:   assets/media/acss-logo-web.png
    	new file:   assets/media/aston-mb-cut.jpg
    	new file:   assets/media/aston-mb-cut1.png
    	new file:   assets/media/aston-mb-cut2.png
    	new file:   assets/media/aston-mb-cut3.png
    	new file:   assets/media/aston-mb-cut4.png
    	new file:   assets/media/aston-mb-cut5.png
    	new file:   assets/media/aston-mb.jpg
    	new file:   assets/media/astonhack-logo.svg
    	modified:   assets/stylesheets/screen.css
    	modified:   favicon.ico
    	new file:   favicon.ico.OLD
    	modified:   index.html

--- static/assets/JS/js.js
+++ static/assets/JS/js.js
@@ -1,46 +1,241 @@
 		$(document).ready(function(){ 
 
+
+
 			//Enable footer address map tool tip
 			$('a[rel="mapTip"]').tooltip();
 
-
-			var onePercentHeadScrolled = parseInt( $("#astonTriangle").css('top') , 10) / 100;
-			var startLeft = parseInt( $("#astonTriangle").css('left') , 10);
-			var beenScrolled = false;
-
-			$(window).scroll(function() {
+			$('#astonmb').removeClass('animated');
 
 
-				var scroll = $(window).scrollTop();
+			reCalcHeaderButtonPosition()
 
 
-				var percentScrolled = scroll / onePercentHeadScrolled;
+			//Calculate the initial Positioning
+			reCalcHeaderButtonsState();
 
-				var newLeftPosPercent = ( percentScrolled / 100 ) * startLeft 
-
-				$("#astonTriangle").css("transform","translate(" + -newLeftPosPercent +  "px , "  + ( scroll * -0.7)  + "px)");
-
-
-
-				if(percentScrolled < 1 && beenScrolled){
-
-							//Remove the login form
-							$('#astonTriangle').removeClass('animated');
-							$('#astonTriangle').removeClass('pulse');
-							$('#astonTriangle').addClass('animated');
-							$('#astonTriangle').addClass('pulse');
-
-							beenScrolled = false;
-
-						} else if(percentScrolled > 150){
-							
-							beenScrolled = true;
-
-						}
+			//Save the menu bar offset in to datatag for later referance
+			var menu = $('.mainNavBar');
+			var origOffsetY = menu.offset().top;
+			$('.mainNavBar').attr('data-origMenuOffsetY', origOffsetY);
+			
+			//Show the header elements, correct poitioning is now done
+			$('#pageHeaderButtons').show();
+			$('#pageHeaderContent').show();
 
 
+			fixPageEndHeight();
+
+
+			/**
+				Parrallx like effect on the mainbuilding as the viewport is scrolled
+				*/
+				$(window).scroll(function() {
+
+
+					var scroll = $(window).scrollTop();
+
+					var vTransformpx = ( scroll * -0.25)
+
+				//Ensure that the building will not be left floating
+				if( parseInt($('#astonmb').css('bottom')) < vTransformpx ){
+					$("#astonmb").css("transform","translate( 0px , "  + vTransformpx + "px)");
+				}
+
+
+				//Recalculate the header buttons visibility
+				reCalcHeaderButtonsState(scroll);
+
+				//Handle the navigation menu bar
+				handleMenuBar();
+
+			});
+
+
+			function handleMenuBar(){
+				//Handle the menu bar
+				var origOffsetY = $('.mainNavBar').attr('data-origMenuOffsetY');
+
+				if($(window).scrollTop() >= (origOffsetY * 0.9)){
+					$('.mainNavBar').css('background-color', '#ecf0f1' );
+					$('.mainNavLinks li').css('margin-right', '5vw' );
+				}
+
+				if ($(window).scrollTop() >= origOffsetY) {
+					$('.mainNavBar').addClass('fixed-top');
+					//add padding to prevent jump with loss of nav bar
+					navBarHeight = parseInt($('.mainNavBar').css('height'));
+					$('.pageContentSection').filter(":first").css('padding-top',  navBarHeight);
+
+
+				} else {
+					$('.mainNavBar').removeClass('fixed-top');
+					$('.mainNavBar').css('background-color', '#FFF' );
+					$('.mainNavLinks li').css('margin-right', '0vw' );
+					$('.pageContentSection').filter(":first").css('padding-top', 0)
+
+				}
+			}
+
+
+			/**
+				Calculates and applies the position of the header buttons
+				*/
+				function reCalcHeaderButtonPosition(scroll){
+
+				//Is a value for scroll passed
+				if(typeof scroll === 'undefined'){ scroll = $(window).scrollTop(); }
+
+				//Display the header buttons in correct pos now page is loaded
+				var headerWidth = parseInt( $("#pageHeader").css('width'));
+				var headerHeight = parseInt( $("#pageHeader").css('height'));
+
+				//Has the width of the rendered button been found
+				if(typeof $('#pageHeaderButtons').attr('data-registrationButtWidth') === 'undefined'){
+
+					//Save the width of the rendered button for later use, this is not accesible when
+					//the button is hidden so must be found and recorded now
+					var headerButtonsWidth = parseInt( $("#pageHeaderButtons").css('width'));
+					$('#pageHeaderButtons').attr('data-registrationButtWidth', headerButtonsWidth);
+					
+				}
+
+				 //Has the height of the rendered content been found
+				 if(typeof $('#pageHeaderContent').attr('data-contentHeight') === 'undefined'){
+
+					//Save the width of the rendered button for later use, this is not accesible when
+					//the button is hidden so must be found and recorded now
+					var headerContentHeight = parseInt( $("#pageHeaderContent").css('height'));
+					$('#pageHeaderContent').attr('data-contentHeight', headerContentHeight);
+					
+				}
+
+
+ 				//Has the width of the rendered content been found
+ 				if(typeof $('#pageHeaderContent').attr('data-contentWidth') === 'undefined'){
+
+					//Save the width of the rendered button for later use, this is not accesible when
+					//the button is hidden so must be found and recorded now
+					var headerContentWidth = parseInt( $("#pageHeaderContent").css('width'));
+					$('#pageHeaderContent').attr('data-contentWidth', headerContentWidth);
+					
+				}
+
+
+				var headerButtonsHeight = parseInt($('#pageHeaderContent').attr('data-contentHeight'));
+				var headerButtonsWidth = $('#pageHeaderButtons').attr('data-registrationButtWidth')
+				var headerButtonsCentre = (headerWidth / 2 ) - (headerButtonsWidth / 2);
+				var headerContentWidth = $('#pageHeaderContent').attr('data-contentWidth');
+				var headerContentCentre = (headerWidth / 2 ) - (headerContentWidth / 2);
+
+				var headerButtonsTop = headerHeight * 0.45;
+				var headerContentTop = headerButtonsTop + headerButtonsHeight;
+
+				$('#pageHeaderButtons').css('left', headerButtonsCentre + 'px' );
+				$('#pageHeaderButtons').css('top', headerButtonsTop + 'px' );
+
+				$('#pageHeaderContent').css('left', headerContentCentre + 'px' );
+				$('#pageHeaderContent').css('top', headerContentTop + 'px' );
+
+				$('#pageHeaderButtons').addClass('animated');
+				$('#pageHeaderButtons').addClass('bounceIn');
+				$('#pageHeaderContent').addClass('animated');
+				$('#pageHeaderContent').addClass('bounceIn');
+			}
+
+			/**
+			Calculates the visibility of the header buttons and hies them when the building is set to scroll overthem
+			sets z-index initialy so they gracefuly leave the view and then is hidden. Reverse is done when scrolling back.
+
+			*/
+			function reCalcHeaderButtonsState(scroll){
+
+				var pageHeaderHeight = parseInt( $("#pageHeader").css('height'));
+				//var requiredClerance = parseInt($('#pageHeaderButtons').css('top')) + parseInt($('#pageHeaderButtons').css('height'));
+				//var mbHidePoint = parseInt( $("#astonmb").css('height')) * 0.35;
+
+				//calculate the position as which to move the buttons to the lower layer, 0.2 was found from trial and error
+				var hideHeaderButtScrollPos = pageHeaderHeight * 0.16; // (pageHeaderHeight - requiredClerance) - mbHidePoint;
+
+				var buttonsVisible = $('#pageHeaderButtons').is(":visible");
+				
+				if(scroll > hideHeaderButtScrollPos){
+					//show
+					$('#pageHeaderButtons').css('z-index', 0);
+				}else{
+					//move z-index so building will slide overtop
+					$('#pageHeaderButtons').css('z-index', 1);
+				}
+
+				if(scroll > pageHeaderHeight && buttonsVisible){
+					//hide header buttons
+					$('#pageHeaderButtons').hide();
+					$('#pageHeaderContent').hide();
+
+				}else if(scroll < pageHeaderHeight && !buttonsVisible){
+					//show header buttons
+					$('#pageHeaderButtons').show();
+					$('#pageHeaderContent').show();
+					//Move to correct position
+					reCalcHeaderButtonPosition();
+
+				}
+
+			}
+
+
+			/**
+				Recalcualate the positioning and apply to the aston uni image
+				*/
+				function recalcOnWindowResize() {
+
+					var headerWidth = parseInt( $("#pageHeader").css('width'));
+					var headerHeight = parseInt( $("#pageHeader").css('height'));
+					var mBImageHeight = parseInt( $("#astonmb").css('height'));
+
+					if(headerWidth > 2000){
+						var mBOffSet = -((headerHeight * 0.05)  + (mBImageHeight * 0.3));
+
+					}else if(headerWidth > 1500){
+						var mBOffSet = -((headerHeight * 0.025)  + (mBImageHeight * 0.3));
+
+					}else{
+						var mBOffSet = -(mBImageHeight * 0.3);
+						
+					}
+
+
+					$('#astonmb').css('bottom', mBOffSet);
+
+					//Fix the new height of the pageEnd
+					fixPageEndHeight();
+
+
+				}
+
+
+
+				$(function () {
+					recalcOnWindowResize();
+
+
+
+					$(window).resize(function() {
+						recalcOnWindowResize();
+						reCalcHeaderButtonPosition();
 					});
+				});
 
+				/*
+				//	Update the height of the pageEnd element to the height of the footer, this allows for the user
+				//	to scroll only the required height.
+				*/
+				function fixPageEndHeight(){
+					var requiredHeight = parseInt($('footer').css('height'));
+					//Apply the height to the pageEnd element 
+					$('#pageEnd').css('height', requiredHeight);
+
+				}
 
 			//?
 			document.cookie = "EssentialTimes=Tuesdays and Thursdays, 6.30 - 8.30. 1st session is Free";
@@ -48,3 +243,9 @@
 
 
 		}); 
+
+
+
+
+
+		
